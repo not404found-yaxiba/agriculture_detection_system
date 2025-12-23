ed9e4097c10ec7456de076fdd7f44bbb6b0f08db
辰HMS <16199421+chen-HMS@user.noreply.gitee.com>
2025-12-23 23:00:31 +0800

    修改README

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 def __init__(self, config_path):
     self.config = self.load_config(config_path)
-    # 保存配置文件的绝对路径
+    # 保存配置文件的路径
     self.config['_config_file'] = os.path.abspath(config_path)
     self.model = None
     self.classes = self.config.get('classes', {})
